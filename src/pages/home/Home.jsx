b921b5be1d2dd8ffd2da5e44e690be18b3ee3fe4
Hike-projects <160869264+Hike-projects@users.noreply.github.com>
2025-04-06 05:26:36 +0000

    New

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+      <div>
+        <h2>Wallpapers Home</h2>
+        <p>Browse and download cool wallpapers!</p>
+      </div>
+    )
+  }
+  
+  export default Home;  
